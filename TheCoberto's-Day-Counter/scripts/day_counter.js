b47024082b932a42e54d832553d6774ede62a1d6
TheCoberto <coby.kelly.ck@gmail.com>
2024-02-25 11:08:48 -0500

    slight refactor

--- TheCoberto's-Day-Counter/scripts/day_counter.js
+++ TheCoberto's-Day-Counter/scripts/day_counter.js
@@ -1,15 +1,13 @@
 import { world, system } from "@minecraft/server";
 
 world.afterEvents.worldInitialize.subscribe(
-    () => {
+    (data) => {
         system.runInterval(
-            () => {
+            (e) => {
                 if (world.getTimeOfDay() == 0)
                 {
-                    let e = system.runInterval(() => {
-                        world.sendMessage("Day: " + world.getDay());
-                        system.clearRun(e)
-                    })
+                    world.sendMessage("Day: " + world.getDay());
+                    system.clearRun(e)
                 };
             },
         )
