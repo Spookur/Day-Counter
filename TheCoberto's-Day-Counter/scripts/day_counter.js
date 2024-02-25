0f667c1624cfb9e9937603fd73e9474e749b0899
TheCoberto <coby.kelly.ck@gmail.com>
2024-02-25 11:01:09 -0500

    undo but remove data var

--- TheCoberto's-Day-Counter/scripts/day_counter.js
+++ TheCoberto's-Day-Counter/scripts/day_counter.js
@@ -1,10 +1,17 @@
 import { world, system } from "@minecraft/server";
 
-world.afterEvents.worldInitialize.subscribe(() => 
-    system.runInterval(() =>
-        world.getTimeOfDay() == 0 && system.runInterval(e => {
-            world.sendMessage("Day: " + world.getDay());
-                system.clearRun(e);
-        })
-    )
+world.afterEvents.worldInitialize.subscribe(
+    () => {
+        system.runInterval(
+            () => {
+                if (world.getTimeOfDay() == 0)
+                {
+                    let e = system.runInterval(() => {
+                        world.sendMessage("Day: " + world.getDay());
+                        system.clearRun(e)
+                    })
+                };
+            },
+        )
+    }
 )
